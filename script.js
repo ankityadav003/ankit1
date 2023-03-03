e23249c7ab13ce480fc86bbac8b58b3f5af7f822
ankityadav003 <123613714+ankityadav003@users.noreply.github.com>
2023-03-03 14:10:54 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,57 @@
+function myFunction() {
+  var x = document.getElementById("a");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+let remove =()=>{
+  let ht=document.querySelector(html)
+}
+function myFunction2() {
+  var chat = document.getElementById("chat");
+  if (chat.style.display === "none") {
+    chat.style.display = "block";
+  } else {
+    chat.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  var c = document.getElementById("cross");
+  if (chat.style.display === "block") {
+    chat.style.display = "none";
+  } else {
+    chat.style.display = "block";
+  }
+}
+function myFunction4() {
+  var chat1 = document.getElementById("chat1");
+  if (chat1.style.display === "none") {
+    chat1.style.display = "block";
+  } else {
+    chat1.style.display = "none";
+  }
+}
+
+
+
+
+
+var type =new Typed('.type',{
+  strings :["Web Developer","Programmer","Freelancer"],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+
+})
+var type =new Typed('.type1',{
+  strings :["Web Developer","Programmer","Freelancer"],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+
+})
